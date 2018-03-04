519a63a9f49cdf03e9451c112288720f551503e2
kinny <1549722424@qq.com>
2018-03-04 18:47:41 +0800

    permutations

--- src/backtracking/Permutations.java
+++ src/backtracking/Permutations.java
@@ -9,7 +9,7 @@ public class Permutations {
 	// 不同元素的排列(已经选择的元素不能再选择) permute, 不同元素的全排列(选择元素可以重复选择) nbits / nchars
 	// 含有相同元素的排列(已经选择的元素不能再选择) permute II , 含有相同元素的全排列(选择元素可以重复选择)
 	// 法一 子问题划分, 自顶向下的递归法, 数学公式清晰. Permute[n] = [insertEveryWhere(elem) for elem in Permute[n-1]]
-	public List<List<Integer>> permute(int[] nums) {
+	public List<List<Integer>> permuteI(int[] nums) {
         return permute(nums, nums.length - 1);
     }
     
@@ -37,14 +37,14 @@ public class Permutations {
         return res;
     }
     
-    // 非递归写法
-    public List<List<Integer>> permute1(int[] nums) {
-    	
+    // 法二　法一的非递归写法．自底向上
+    public List<List<Integer>> permuteI1(int[] nums) {
+        return null;
     }
     
-    // 法二 DFS 采用深度优先搜索该问题的解空间树, 将解存在一个数组 path 中, 从第一个位置开始选择剩余可以选择的元素,直到选到最后一个元素;
+    // 法三 DFS 采用深度优先搜索该问题的解空间树, 将解存在一个数组 path 中, 从第一个位置开始选择剩余可以选择的元素,直到选到最后一个元素;
     // 搜索返回的时候记住必须标记会被选择的元素为 还未选择,从新开始搜索树的下一个兄弟分支;
-    public List<List<Integer>> permute2(int[] nums) {
+    public List<List<Integer>> permuteI2(int[] nums) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         int n = nums.length;
         boolean[] leftedNums = new boolean[n];
@@ -73,6 +73,38 @@ public class Permutations {
         }
     }
     
+    // 法四　剑指offer 的版本．想法是把字符串分成左右两部分，第一部分是第一个字符，第二部分是后面的字符串．
+    // 第一个字符可以有多种不同的选择，从第一个到最后一个(如果没有重复元素的话), 选定一个之后，第二部分就可以作为相同性质的子问题，
+    // 这里对全排列的另一种演算方式是交换任意两个位置元素的过程，由基础排列开始，第一个元素可以和第二部分任意元素交换，
+    // 交换之后子问题一样的方式进行, 但是子问题求完之后再换回来就可以恢复原样
+    // 这种方法的好处是有的，就是当含有重复元素的时候，(可以对字符串排序)
+    public ArrayList<String> Permutation(String str) {
+        char[] chs = str.toCharArray();
+        //Arrays.sort(chs);
+        StringBuilder sb = new StringBuilder(String.valueOf(chs));
+        ArrayList<String> res = new ArrayList<String>();
+        if (str == null || str.length() == 0) return res;
+        permute(res, sb, 0);
+        return res;
+     }
+     
+     public void permute(ArrayList<String> res, StringBuilder sb, int pos) {
+        if (pos == sb.length()) {
+            res.add(sb.toString());
+            return;
+        }
+        for (int i = pos; i < sb.length(); i++) {
+        	if (i != pos && sb.charAt(i) == sb.charAt(pos)) continue; // 在不同的位置上相同的值,交换了也没用
+            char temp = sb.charAt(i);
+            sb.setCharAt(i, sb.charAt(pos));
+            sb.setCharAt(pos, temp);
+            permute(res, sb, pos + 1);
+            temp = sb.charAt(i);
+            sb.setCharAt(i, sb.charAt(pos));
+            sb.setCharAt(pos, temp);
+        }
+        return;
+     }
     
  // T(n) = 2 * T(n-1) + O(1); NP 2^n
  	public void nBits(int n, int[] A) {
--- src/sort/SearchRotated.java
+++ src/sort/SearchRotated.java
@@ -1,6 +1,11 @@
 package sort;
 
 public class SearchRotated {
+	
+	// [33. Search in Rotated Sorted Array](https://leetcode.com/problems/search-in-rotated-sorted-array/description/)
+	// 1 2 3 4 5 6 7 8 9  a[m] < a[r] a[m:r] sorted
+	// 3 4 5 6 7 8 9 1 2  a[m] > a[r] a[l:m] sorted
+	// 8 9 1 2 3 4 5 6 7  a[m] < a[r] 
 	public int search(int[] nums, int target) {
         if (nums == null || nums.length == 0) return -1;
         int low = 0;
@@ -19,4 +24,84 @@ public class SearchRotated {
         }
         return -1;
     }
+	
+	// [81. Search in Rotated Sorted Array II](https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/)
+	// 当有重复数字，会存在a[m] = a[r]的情况。此时右半序列a[m-1 : r]可能是sorted，也可能并没有sorted，如下例子。
+	// 3 1 2 3 3 3 3 
+	// 3 3 3 3 1 2 3
+	// 因此a[m] = a[r] != target时，无法排除一半的序列，而只能排除掉a[r]. 只能知道 a[r] 一定不是解.
+	public boolean search1(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (nums[m] == target) return true;
+            if (nums[m] < nums[r]) {
+                if (nums[m] <= target && nums[r] >= target) l = m + 1;
+                else r = m - 1;
+            }
+            else if (nums[m] > nums[r]) {
+                if (nums[m] >= target && nums[l] <= target)  r = m - 1;
+                else l = m + 1;
+            }
+            // 去除重复的数据,这样就不用考虑那些重复的数的影响, [3,1,1] 3 如果不加入这个, 直接判断 nums[m] >= nums[r] 就断定左边有序而右边无序,
+            // 而此时 3 并不满足 3 >=3 && 3 <= 1, 导致条件跳转到 右边去找.
+            else {
+                r --;
+            }
+        }
+        return false;
+    }
+	
+	// [153. Find Minimum in Rotated Sorted Array](https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/)
+	public int findMin(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = low;
+        // 最小值和最大值都在分解点的位置，并且最大值在左，最小值在右
+        while (nums[low] >= nums[high]) { // 满足条件则说明旋转过
+            mid = (low + high) / 2;
+            if (high - low == 1) return nums[high];
+            if (nums[mid] >= nums[high]) {
+                low = mid;
+            }
+            else {
+                high = mid;
+            }
+        }
+        return nums[mid];
+    }
+	
+	//[154. Find Minimum in Rotated Sorted Array II-hard](https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/description/)
+	public int minNumberInRotateArray(int [] array) {
+        if (array.length == 1) return array[0];
+        int low = 0;
+        int high = array.length - 1;
+        int mid = high;
+        while (array[low] >= array[high]) {
+            mid = (low + high) / 2;
+            if (high - low == 1) return array[high];
+            if (array[mid] == array[high] && array[mid] == array[low]) { // 针对有重复数据的情况，无法判断mid属于左边还是右边，需要顺序求解
+                return minOrder(array, low, high);
+            }
+            if (array[mid] >= array[high]) {
+                low = mid;
+            }
+            else if (array[mid] <= array[high]) {
+                high = mid;
+            }
+        }
+        return array[mid];
+    }
+    public int minOrder(int[] array, int low, int high) {
+        int min = array[low];
+        for (int i = low + 1; i <= high; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+}}
 }
